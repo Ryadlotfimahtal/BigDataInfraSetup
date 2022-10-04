655087c15877818fab8be100de3796773164bedb
Ryad Lotfi <86742282+Ryadlotfimahtal@users.noreply.github.com>
2022-10-04 12:03:56 +0200

    Big fix

--- hadoop_install.sh
+++ hadoop_install.sh
@@ -6,7 +6,7 @@
 sudo apt install ssh
 sudo apt install pdsh
 
-echo 'export PDSH_RCMD_TYPE=ssh' >> .bashrc
+echo 'export PDSH_RCMD_TYPE=ssh' >> ~/.bashrc
 
 
 #Generate current host key and authorize it for ssh
@@ -21,13 +21,13 @@ tar xzf hadoop-2.7.3.tar.gz
 sudo mv hadoop-2.7.3 /usr/local/hadoop
 
 #Configure environment variables
-echo 'export HADOOP_HOME=/usr/local/hadoop' >> .bashrc
-echo 'export HADOOP_INSTALL=$HADOOP_HOME' >> .bashrc
-echo 'export HADOOP_MAPRED_HOME=$HADOOP_HOME' >> .bashrc
-echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' >> .bashrc
-echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' >> .bashrc
-echo 'export YARN_HOME=$HADOOP_HOME' >> .bashrc
-echo 'export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native' >> .bashrc
-echo 'export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin' >> .bashrc
-source .bashrc
+echo 'export HADOOP_HOME=/usr/local/hadoop' >> ~/.bashrc
+echo 'export HADOOP_INSTALL=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_MAPRED_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export YARN_HOME=$HADOOP_HOME' >> ~/.bashrc
+echo 'export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native' >> ~/.bashrc
+echo 'export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin' >> ~/.bashrc
+source ~/.bashrc
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /usr/local/hadoop/etc/hadoop/hadoop-env.sh
